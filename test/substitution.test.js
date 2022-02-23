e4f97b803a2f45bf0832ae5843d0cb30267c91f4
vinyl704 <mpbarber@mymail.gaston.edu>
2022-02-23 14:18:44 -0500

    finished. . . i think

--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,1 +1,49 @@
+/*
+    The input could include spaces and letters as well as special characters such as #, $, *, etc.
+Spaces should be maintained throughout.
+Capital letters can be ignored.
+The alphabet parameter must be a string of exactly 26 characters, which could include special characters such as #, $, *, etc. Otherwise, it should return false.
+All the characters in the alphabet parameter must be unique. Otherwise, it should return false.
+*/
+
 // Write your tests here!
+const {expect} = require('chai');
+const {substitution} = require('../src/substitution');
+
+describe('tests for substitution() encoder/decoder',()=>{
+    it('should return false if no alphabet provided',()=>{
+        const message = "Oh no, our table, it's broken";
+        const actual = substitution(message);
+        expect(actual).to.be.false;
+    })
+    
+    it('should return false if alphabet not exactly 26 characters',()=>{
+        const message = "Hey there";
+        const alphabet = "d38n.-0(tuyqrwe";
+        const actual = substitution(message,alphabet);
+        expect(actual).to.be.false;
+    })
+
+    it('should maintain spaces',()=>{
+        const message = "Hey there";
+        const alphabet = "d38n.-0(tuyqrwepx6m/,71294";
+        const actual = substitution(message,alphabet);
+        const expected = "(.9 /(.6.";
+        expect(actual).to.equal(expected);
+    })
+    it('should maintain spaces',()=>{
+        const message = "(.9 /(.6.";
+        const expected = "hey there";
+        const alphabet = "d38n.-0(tuyqrwepx6m/,71294";
+        const actual = substitution(message,alphabet,false);
+        expect(actual).to.equal(expected);
+    })
+
+    it('should return false if alphabet contains duplicates',()=>{
+        const message = "Hey there";
+        const alphabet = "d38n.-0(tuyqrwepx6n/,71294";
+        const actual = substitution(message,alphabet);
+        const expected = "(n9 /(n6n";
+        expect(actual).to.be.false;
+    })
+})
